8433c6ac7d9a4f32f4596ff123fb784f4979f301
Matias <matias_haugum@hotmail.com>
2023-02-18 13:15:33 +0100

    Updated compose

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-echo "$PWD"22
 echo Pongdog installation! 
 echo Collecting dependencies...
-#apt-get update && apt-get upgrade
-#apt install libasound2-dev
-#curl -sSL https://get.docker.com | sh
-#sudo usermod -aG docker pi
-#echo Installation completed. Reboot in 5 seconds.
-#sleep 5
-#reboot
+apt-get update && apt-get upgrade
+apt install libasound2-dev
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker pi
+echo Installation completed. Reboot in 5 seconds.
+sleep 5
+reboot
